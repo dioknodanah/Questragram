3213b678049de10a84948ffaffb614a920d5f276
danahdiokno <danah.edd_diokno@questronix.com.ph>
2024-01-31 11:31:15 +0800

    Initial commit

---
+++ questagram/api/controllers/post.js
@@ -0,0 +1,56 @@
+import {db} from "../connect.js";
+import jwt from "jsonwebtoken";
+import moment from "moment";
+
+
+export const getPosts =(req, res) => {
+    
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).json("Not logged in!");
+
+    jwt.verify(token, "secretkey", (err, userInfo)=>{
+        if (err) return res.status(403).json("Token is not valid!");
+    
+
+        const q = `SELECT p.*, u.id AS userId, username FROM post AS p JOIN users AS u ON (u.id = p.userId)`;
+
+        db.query(q, [userInfo.id], (err,data)=>{
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data);
+        });
+
+    });
+    
+};
+
+export const addPost =(req, res) => {
+    
+    const token = req.cookies.accessToken;
+    if (!token) return res.status(401).json("Not logged in!");
+
+    jwt.verify(token, "secretkey", (err, userInfo)=>{
+        if (err) return res.status(403).json("Token is not valid!");
+        
+        const img = req.body.img || "";
+
+        // const key = Object.keys(img);
+        // console.log(key)
+
+        const q = "INSERT INTO post (`upload_date`, `img`, `caption`, `userId` ) VALUES (?)";
+
+        const values = [
+            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            img, 
+            req.body.caption,
+            userInfo.id
+        ];
+
+        console.log('Inserting values:', values);
+
+        db.query(q, [values], (err,data)=>{
+            if (err) return res.status(500).json(err);
+            return res.status(200).json("Post has been created!");
+        });
+
+    });
+};
---
+++ questagram/api/routes/post_image.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { } from "../controllers/post_image.js"
+
+const router = express.Router()
+
+router.get("",)
+
+
+
+export default router
